d511288229b5afaf06e08c49c8c20de97cc93e41
cbttestuser1 <cbttestuser1@cbttestuser1.gmail.com>
2017-11-11 19:55:29 -0500

    check

--- src/test/java/com/cbt/AppTest.java
+++ src/test/java/com/cbt/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. This is using the old junit
  */
 public class AppTest 
     extends TestCase
